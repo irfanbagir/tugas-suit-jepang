b101ac8f749d7eb3d1e9ba8ba6a3a024812456cf
Muhammad Irfan <irfan.bagir@gmail.com>
2021-04-29 01:21:42 +0700

    Perapihan fungsi player pick

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,17 @@
+import utils.PlayerPick
+
+class App {
+    companion object{
+        @JvmStatic
+        fun main(args: Array<String>) {
+            headerGame()
+            PlayerPick().pOnePick()
+        }
+
+        fun headerGame(){
+            println("=========================")
+            println("Game Suit Jepang Terminal")
+            println("=========================")
+        }
+    }
+}
---
+++ src/main/kotlin/utils/RumusSuit.kt
@@ -0,0 +1,40 @@
+package utils
+
+import kotlin.system.exitProcess
+
+class RumusSuit {
+    fun rumusSuit(playerOne: String, playerTwo: String) {
+        if (playerOne == playerTwo) {
+            println()
+            println("Hasil:")
+            println("Draw!")
+        } else if (playerOne == "gunting" && playerTwo == "kertas") {
+            println()
+            println("Hasil:")
+            println("Player 1 Wins!")
+        } else if (playerOne == "batu" && playerTwo == "gunting") {
+            println()
+            println("Hasil:")
+            println("Player 1 Wins!")
+        } else if (playerOne == "kertas" && playerTwo == "batu") {
+            println()
+            println("Hasil:")
+            println("Player 1 Wins!")
+        } else {
+            println()
+            println("Hasil:")
+            println("Player 2 Wins!")
+        }
+        println()
+        print("Main lagi?(Y/N):")
+        val a = readLine().toString().toLowerCase()
+        if (a == "y"){
+            PlayerPick().pOnePick()
+        }
+        else{
+            println("Thanks for Playing!!")
+            exitProcess(0)
+        }
+    }
+
+}
