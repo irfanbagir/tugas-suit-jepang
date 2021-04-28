a67819fdf2309e83d86ebcde41cce547c8d78704
Muhammad Irfan <irfan.bagir@gmail.com>
2021-04-29 01:27:18 +0700

    Perapihan fungsi dan Perubahan App

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,17 +1,19 @@
 import utils.PlayerPick
 
 class App {
-    companion object{
-        @JvmStatic
-        fun main(args: Array<String>) {
-            headerGame()
-            PlayerPick().pOnePick()
-        }
-
-        fun headerGame(){
-            println("=========================")
-            println("Game Suit Jepang Terminal")
-            println("=========================")
-        }
+companion object {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        headerGame()
+        PlayerPick().pOnePick()
     }
+
+
+    fun headerGame() {
+        println("=========================")
+        println("Game Suit Jepang Terminal")
+        println("=========================")
+        println("Pilih antara Gunting, Batu, Atau Kertas")
+    }
+}
 }
