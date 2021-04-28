e27d90ff25e601448d51bf8e68a67bb53286992c
Muhammad Irfan <irfan.bagir@gmail.com>
2021-04-29 01:20:18 +0700

    Perapihan fungsi player pick

---
+++ src/main/kotlin/utils/PlayerPick.kt
@@ -0,0 +1,41 @@
+package utils
+
+var playerOne : String = ""
+var playerTwo : String = ""
+class PlayerPick {
+    fun pOnePick() {
+        print("Player One silahkan pilih :")
+        playerOne = readLine().toString().toLowerCase()
+        when (playerOne) {
+            "gunting" -> {
+                pTwoPick()
+            }
+            "batu" -> pTwoPick()
+            "kertas" -> pTwoPick()
+
+            else -> {
+                println("Anda tidak memilih antara gunting, batu atau kertas")
+                pOnePick()
+            }
+        }
+
+    }
+
+    fun pTwoPick() {
+        print("Player Two silahkan pilih :")
+        var playerTwo = readLine().toString().toLowerCase()
+        when (playerTwo) {
+            "gunting" -> RumusSuit().rumusSuit(playerOne, playerTwo)
+            "batu" -> RumusSuit().rumusSuit(playerOne, playerTwo)
+            "kertas" -> RumusSuit().rumusSuit(playerOne, playerTwo)
+
+            else -> {
+                println("Anda tidak memilih antara gunting, batu atau kertas")
+                pTwoPick()
+            }
+        }
+
+    }
+}
+
+
